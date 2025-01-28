db79492734397703fa274a92ea14a04982e82bd8
EthanWThomas <111320646+EthanWThomas@users.noreply.github.com>
2025-01-27 21:40:48 -0700

    Initial Commit

---
+++ Brick Finder/Brick_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brick_FinderApp.swift
+//  Brick Finder
+//
+//  Created by Ethan Thomas on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Brick_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
